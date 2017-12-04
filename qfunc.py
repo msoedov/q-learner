7899a8960b70264ae6c93ac0dcb03b0d28d90b46
Alex Miasoiedov <msoedov@gmail.com>
2017-12-04 13:37:46 -0500

    Update qfunc.py

--- qfunc.py
+++ qfunc.py
@@ -23,7 +23,7 @@ class QFunc:
     learning_rate = 0.1
     gamma_factor = 0.9
     epsilon = 0.1
-    exploration_decay = 1.00001
+    exploration_decay = 1.00005
 
     def __init__(self, action_space):
         self.action_space = action_space
@@ -31,7 +31,8 @@ class QFunc:
 
     def _hash_word_state(self, state: np.array) -> int:
         """
-        Hashes word state of multy dim np.array into an int
+        Hashes word state of multy dim np.array into an int. Will result a low memory footprint for
+        hash table
         """
 
         return hash(tuple(state.flatten()))
@@ -39,14 +40,20 @@ class QFunc:
     def size(self) -> int:
         return len(self.q_table)
 
-    def learn(self, old_state: np.array, action, reward: int, new_state: np.array) -> None:
+    def learn(self,
+              old_state: np.array,
+              action: int,
+              reward: int,
+              new_state: np.array) -> None:
         """
         Definition of Q-learning taken from
 
         https://en.wikipedia.org/wiki/Q-learning
         """
-        q_old_state = self.q_table[self._hash_word_state(old_state)].get(action, random.randint(1, 10))
-        q_new_state_max = max(self.q_table[self._hash_word_state(new_state)] or [random.randint(1, 10)])
+        q_old_state = self.q_table[self._hash_word_state(old_state)].get(
+            action, random.randint(1, 10))  # use random to init table if value does not exists
+        q_new_state_max = max(self.q_table[self._hash_word_state(new_state)] or
+                              [random.randint(1, 10)])
         self.q_table[self._hash_word_state(old_state)][action] = q_old_state + self.learning_rate * \
             (reward + self.gamma_factor * q_new_state_max - q_old_state)
 
@@ -62,11 +69,12 @@ class QFunc:
         if random.random() > self.epsilon:
             return self.action_space.sample()
         else:
-            action = max(self.q_table[self._hash_word_state(state)] or [self.action_space.sample()])
+            action = max(self.q_table[self._hash_word_state(state)] or
+                         [self.action_space.sample()])
             return action
 
 
-if __name__ == '__main__':
+def train():
     env = gym.make('SpaceInvaders-v0')
     outdir = '/tmp/q-space-func'
     # env = wrappers.Monitor(env, directory=outdir, force=True)
@@ -78,21 +86,27 @@ if __name__ == '__main__':
     max_score = 0
     all_time_max = 0
     for i in range(episode_count):
-        ob = env.reset()
+        state = env.reset()
         print('#' * 50)
         print("Current score", max_score)
         print("Max score", all_time_max)
         print("Game number #", i)
         print("Observed states", q_learner.size())
-        print("Exploration factor {:.1f}%".format((1 - q_learner.epsilon) * 100))
-        print("Q function decisions factor {:.1f}%".format((q_learner.epsilon) * 100))
+        print("Exploration factor {:.1f}%".format((1 - q_learner.epsilon) *
+                                                  100))
+        print("Q function decisions factor {:.1f}%".format((q_learner.epsilon)
+                                                           * 100))
         all_time_max = max(all_time_max, max_score)
         max_score = 0
         while True:
-            action = q_learner.make_decision(ob)
-            ob_ = ob
-            ob, reward, done, _ = env.step(action)
-            q_learner.learn(old_state=ob_, action=action, reward=reward, new_state=ob)
+            action = q_learner.make_decision(state)
+            state_ = state
+            state, reward, done, _ = env.step(action)
+            q_learner.learn(
+                old_state=state_,
+                action=action,
+                reward=reward,
+                new_state=state)
             max_score += reward
             if done:
                 break
@@ -100,3 +114,7 @@ if __name__ == '__main__':
     with open('qfunc.pickle', 'wb') as handle:
         pickle.dump(q_learner, handle, protocol=pickle.HIGHEST_PROTOCOL)
     env.close()
+
+
+if __name__ == '__main__':
+    train()
